fd10dfca199ec55fdad86a00f86742778c584719
shravanatirtha <shravz.ceo@gmail.com>
2021-05-03 18:10:08 +0530

    modified

--- index.js
+++ index.js
@@ -1,43 +1,36 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.listen("5000", function (req, res) {
   console.log("node server on http://localhost:5000");
 });
-app.get("/api", function (req, res) {
+app.use(express.static("public"));
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
-var message = "Add/Edit a date in address bar as /yyyy-mm-dd";
-app.get("/api/timestamp", function (req, res) {
-  res.json({
-    unix: Date.now(),
-    utc: Date,
-    message: message,
-  });
-});
 
-app.get("/api/timestamp/:date_string", (req, res) => {
-  let dateString = req.params.date_string;
-  if (/\d{5,}/.test(dateString)) {
-    const dateInt = parseInt(dateString);
+app.get("/api/:dateString?", (req, res) => {
+  const dateString = req.params.dateString;
+  let date;
+  if (!dateString) {
+    date = new Date();
+  } else {
+    if (!isNaN(dateString)) {
+      date = new Date(parseInt(dateString));
+    } else {
+      date = new Date(dateString);
+    }
+  }
+  if (date.toString() === "Invalid Date") {
     res.json({
-      unix: dateInt,
-      utc: new Date(dateInt).toUTCString(),
-      message: message,
+      error: date.toString(),
     });
   } else {
-    let dateObject = new Date(dateString);
-
-    if (dateObject.toString() === "Invalid Date") {
-      res.json({
-        error: "Invalid Date",
-      });
-    } else {
-      res.json({
-        unix: dateObject.valueOf(),
-        utc: dateObject.toUTCString(),
-      });
-    }
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    });
   }
 });
